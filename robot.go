87c4359b62d0f59a0a792977733d61cff0edf38c
Tiger <tuwanqi@gmail.com>
2022-06-15 17:41:36 +0800

    feat: 企微机器人增加文件上传与推送支持

--- robot.go
+++ robot.go
@@ -3,13 +3,17 @@ package wework
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"mime/multipart"
 	"net/http"
+	"os"
 
 	json "github.com/json-iterator/go"
 )
 
 const robotApi = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key="
+const robotUploadApi = "https://qyapi.weixin.qq.com/cgi-bin/webhook/upload_media?type=file&key="
 
 // robotPost http
 func robotPost(robotId string, data interface{}) (respBytes []byte, err error) {
@@ -75,4 +79,79 @@ func RobotPushMarkdown(robotId string, content string) error {
 		return err
 	}
 	return nil
+}
+
+// RobotPushFile 推送文件消息
+// robotId		 机器人id
+// mediaId		 文件id
+func RobotPushFile(robotId string, mediaId string) error {
+	msg := &RobotFileMsg{MsgType: "file"}
+	msg.File.MediaId = mediaId
+	res, err := robotPost(robotId, msg)
+	if err != nil {
+		return err
+	}
+	var errRes errResponse
+	err = json.Unmarshal(res, &errRes)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// RobotUploadFile 上传文件至机器人空间, 返回media_id
+// robotId		机器人id
+// filename		文件路径
+func RobotUploadFile(robotId, filepath, filename string) (string, error) {
+	payload := &bytes.Buffer{}
+	writer := multipart.NewWriter(payload)
+	fileWriter, err := writer.CreateFormFile("media", filename)
+	if err != nil {
+		fmt.Println("error writing to buffer")
+		return "", err
+	}
+
+	f, err := os.Open(filepath)
+	if err != nil {
+		fmt.Println("error opening file")
+		return "", err
+	}
+	defer f.Close()
+
+	_, err = io.Copy(fileWriter, f)
+	if err != nil {
+		return "", err
+	}
+	err = writer.Close()
+	if err != nil {
+		return "", err
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s%s", robotUploadApi, robotId), payload)
+
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	var errRes RobotUploadResponse
+	err = json.Unmarshal(body, &errRes)
+	if err != nil {
+		return "", err
+	}
+	if errRes.MediaId == "" {
+		return "", fmt.Errorf("failed to upload files")
+	}
+	return errRes.MediaId, nil
 }
--- robot_test.go
+++ robot_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 // https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=15be2cab-6fc2-42e9-a135-99595acf19e3
 
-const robot = "xx1xx2xx-xxx3-xxx4-xxx5-99595acf19e3"
+const robot = "15be2cab-6fc2-42e9-a135-99595acf19e3"
 
 func TestRobotPushText(t *testing.T) {
 	err := RobotPushText(robot, "> test", "xxx@xxxxxx.com")
@@ -23,6 +23,17 @@ func TestRobotPushMarkdown(t *testing.T) {
 		t.Error(err)
 	}
 	err = RobotPushMarkdown(robot, "> test")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestRobotPushFile(t *testing.T) {
+	mediaId, err := RobotUploadFile(robot, "/Users/twq/Downloads/峰值低于5%的机器总表-0519.xlsx", "test.xlsx")
+	if err != nil {
+		t.Error(err)
+	}
+	err = RobotPushFile(robot, mediaId)
 	if err != nil {
 		t.Error(err)
 	}
